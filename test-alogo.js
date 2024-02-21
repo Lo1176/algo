a08e14712b6b8e425b582cbae215f76621f5b165
Laurent Binder <laurentbinder@gmail.com>
2024-02-21 12:49:03 +0100

    add confirmEnding

--- test-alogo.js
+++ test-alogo.js
@@ -46,11 +46,19 @@ function largestOfFour(arr) {
   return arrResult;
 }
 
-console.log(
-  largestOfFour([
-    [17, 23, 25, 12],
-    [25, 7, 34, 48],
-    [4, -10, 18, 21],
-    [-72, -3, -17, -10],
-  ])
-);
+// console.log(
+//   largestOfFour([
+//     [17, 23, 25, 12],
+//     [25, 7, 34, 48],
+//     [4, -10, 18, 21],
+//     [-72, -3, -17, -10],
+//   ])
+// );
+
+function confirmEnding(str, target) {
+  const endLength = target.length;
+  const lastLetters = str.slice(-endLength);
+  return lastLetters === target && true;
+}
+
+console.log(confirmEnding('Bastian', 'tian'));
