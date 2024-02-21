0058249b18feccb564b391d9711788f6121842c4
Laurent Binder <laurentbinder@gmail.com>
2024-02-21 12:12:43 +0100

    add findLongestWordLength

--- test-alogo.js
+++ test-alogo.js
@@ -6,15 +6,27 @@ console.log(reverseString('hello'));
 
 const toto = (num) => {
   let arr = [];
+  let result = 1;
   for (let index = 1; index <= num; index++) {
     arr.push(index);
   }
-  console.log('🚀 ~ toto ~ arr:', arr);
-  let result = 1;
   for (let i = 0; i < arr.length; i++) {
     result = result * arr[i];
   }
   return result;
 };
 
-toto(5);
+console.log('toto: ', toto(5));
+
+function findLongestWordLength(str) {
+  let arrayOfWords = str.split(' ');
+  let result = '';
+  arrayOfWords.map((word) => {
+    return (result = word.length > result.length ? word : result);
+  });
+  return result.length;
+}
+
+console.log(
+  findLongestWordLength('The quick brown fox jumped over the lazy dog')
+);
