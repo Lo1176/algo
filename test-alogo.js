385ed382992e7cca90343824dc7a1f730382f5a2
Laurent Binder <laurentbinder@gmail.com>
2024-02-23 11:17:59 +0100

    add booWho

--- test-alogo.js
+++ test-alogo.js
@@ -112,4 +112,17 @@ function findElement(arr, func) {
   return undefined;
 }
 
-console.log(findElement([1, 5, 3, 7], (num) => num % 2 === 0));
+// console.log(findElement([1, 2, 3, 4], (num) => num % 2 === 0));
+
+/**
+ * Check if a value is classified as a boolean primitive. Return true or false.
+ * Boolean primitives are true and false.
+ */
+
+function booWho(bool) {
+  return typeof bool === 'boolean';
+}
+
+console.log(booWho(false));
+console.log(booWho('toto'));
+console.log(true && false);
