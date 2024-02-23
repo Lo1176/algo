2cd7a4ddfdbd548aaaa1936cb48ac6b020f2e7cc
Laurent Binder <laurentbinder@gmail.com>
2024-02-23 11:03:46 +0100

    add findElement

--- test-alogo.js
+++ test-alogo.js
@@ -97,4 +97,19 @@ function truncateString(str, num) {
   }
 }
 
-console.log(truncateString('A-tisket a-tasket A green and yellow basket', 8));
+// console.log(truncateString('A-tisket a-tasket A green and yellow basket', 8));
+
+/**
+ * Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'.
+ * This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.
+ */
+function findElement(arr, func) {
+  for (let i = 0; i < arr.length; i++) {
+    if (func(arr[i])) {
+      return arr[i];
+    }
+  }
+  return undefined;
+}
+
+console.log(findElement([1, 5, 3, 7], (num) => num % 2 === 0));
