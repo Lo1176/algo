69a2b6ebfc4814a841f20354bc934cdb4ba8b4fc
Laurent Binder <laurentbinder@gmail.com>
2024-02-26 22:04:40 +0100

    add frankenSplice

--- test-alogo.js
+++ test-alogo.js
@@ -140,4 +140,22 @@ function titleCase(str) {
   return arr.join(' ');
 }
 
-console.log(titleCase("I'm a little tea pot"));
+// console.log(titleCase("I'm a little tea pot"));
+
+/**
+ * Slice and Splice
+ * You are given two arrays and an index.
+ * Copy each element of the first array into the second array, in order.
+ * Begin inserting elements at index n of the second array.
+ * Return the resulting array. The input arrays should remain the same after the function runs.
+ */
+function frankenSplice(arr1, arr2, n) {
+  // permet de créer une nouvel array sans modifier arr2 par la suite
+  let result = [...arr2];
+  result.splice(n, 0, arr1);
+  // concat permet de faire un flatten comme en Ruby
+  return [].concat(...result);
+}
+
+console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
+console.log(frankenSplice([1, 2, 3], [4, 5], 1));
