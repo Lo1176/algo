624d332ecbcfdf13c085e430af9956bde0f48c15
Laurent Binder <laurentbinder@gmail.com>
2024-02-28 20:27:40 +0100

    add getIndexToIns

--- test-alogo.js
+++ test-alogo.js
@@ -170,4 +170,48 @@ function bouncer(arr) {
   return result.filter(Boolean);
 }
 
-console.log(bouncer([7, 'ate', '', false, 9]));
+// console.log(bouncer([7, 'ate', '', false, 9]));
+
+/** 
+ * Where do I Belong
+Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. 
+The returned value should be a number.
+For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] 
+and 19 is less than 20 (index 2) and greater than 5 (index 1).
+
+ */
+
+function getIndexToIns(arr, num) {
+  arr.sort((a, b) => a - b);
+  if (arr.indexOf(num) !== -1) {
+    console.log('🚀 ~ getIndexToIns ~ num:', arr.indexOf(num));
+    return arr.indexOf(num);
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > num) {
+        console.log('ici index: ', i);
+        return i;
+      }
+    }
+  }
+  return arr.length;
+}
+console.log(getIndexToIns([40, 60], 61));
+
+function getIndexToIns2(arr, num) {
+  arr.sort((a, b) => a - b);
+  // Find the index to insert the number
+  let index = arr.findIndex((element) => num <= element);
+
+  // If the number is greater than all elements in the array
+  // Set index to the length of the array
+  if (index === -1) {
+    index = arr.length;
+  }
+  return index;
+}
+
+console.log(getIndexToIns2([40, 60], 61));
+console.log(getIndexToIns2([1, 2, 3, 4], 1.5)); // Output: 1
+console.log(getIndexToIns2([20, 3, 5], 19)); // Output: 2
