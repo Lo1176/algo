de2c9f5cf769770cb652b5bbcee61ab95562a51c
Laurent Binder <laurentbinder@gmail.com>
2024-02-29 16:14:00 +0100

    removed console.log

--- test-alogo.js
+++ test-alogo.js
@@ -228,7 +228,6 @@ function mutation(arr) {
   const firstString = arr[0].toLowerCase();
   const secondString = arr[1].toLowerCase();
   for (let i = 0; i < secondString.length; i++) {
-    console.log(firstString.indexOf(secondString[i]));
     if (firstString.indexOf(secondString[i]) === -1) return false;
   }
   return true;
